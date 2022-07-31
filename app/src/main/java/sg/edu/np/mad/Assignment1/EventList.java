d237edb59b3d568583c51e0c808be580bc375f09
irenepxp <S10208484@connect.np.edu.sg>
2022-08-01 02:16:46 +0800

    on the eventadapter layout name the ID as event layout

--- app/src/main/java/sg/edu/np/mad/Assignment1/EventList.java
+++ app/src/main/java/sg/edu/np/mad/Assignment1/EventList.java
@@ -2,12 +2,17 @@ package sg.edu.np.mad.Assignment1;
 
 public class EventList {
 
-    String img_url, event_info, time;
+    String img_url, event_info, time, description;
 
-    public EventList(String img_url, String event_info, String time) {
+    public EventList(String img_url, String event_info, String description,String time) {
         this.img_url = img_url;
         this.event_info = event_info;
         this.time = time;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getImg_url() {
